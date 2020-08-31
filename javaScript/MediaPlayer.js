cb3bacf31db411e0e504c7d0d69eea77c67eaaee
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-31 12:41:54 -0500

    Creacion Modulo encargado de reproduccion de video

---
+++ javaScript/MediaPlayer.js
@@ -0,0 +1,44 @@
+function MediaPlayer (config) {
+  this.media = config.video;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach(plugin => {
+    plugin.run(this);
+  });
+}
+
+MediaPlayer.prototype.togglePlay = function () {
+  if (this.media.paused)
+    this.play();
+  else 
+    this.pause();
+}
+
+MediaPlayer.prototype.toggleMute = function () {
+  if (!this.media.muted)
+    this.mute();
+  else 
+    this.unmute();
+}
+
+MediaPlayer.prototype.play = function () {
+  this.media.play();
+}
+
+MediaPlayer.prototype.pause = function () {
+  this.media.pause();
+}
+
+MediaPlayer.prototype.mute = function () {
+  this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmute = function () {
+  this.media.muted = false;
+}
+
+export default MediaPlayer;
